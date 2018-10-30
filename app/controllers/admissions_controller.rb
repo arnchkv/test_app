782bc8b28d19f66c181e7cfc602cad0f41c52ab8
Udit Chakraborty <udit.chakraborty8@gmail.com>
2018-10-30 17:17:42 +0530

    edit show, all done

--- app/controllers/admissions_controller.rb
+++ app/controllers/admissions_controller.rb
@@ -6,15 +6,35 @@ class AdmissionsController < ApplicationController
     def create
         #render plain: params[:admission].inspect
         @admissions = Admission.new(admission_params)
-        if @admissions.save
-                flash[:notice] = "admission done!"
-                redirect_to admissions_path(@admissions)
-        else
+                if @admissions.save
+                        flash[:notice] = "admission done!"
+                        redirect_to admission_path(@admissions)
+                else
                 render 'new'
+                end
+
         end
 
-end
-    
+        def edit
+                @admissions = Admission.find(params[:id])
+        end
+
+        def show
+                @admissions = Admission.find(params[:id])
+        end
+        def update
+                @admissions = Admission.find(params[:id])
+                if @admissions.update(admission_params)
+                        flash[:notice] = "update successful"
+                        redirect_to admission_path(@admissions)
+                else
+                        render 'edit'
+                end
+        end
+        def index
+                @admissions = Admission.all
+                 
+        end
 
     private
         def admission_params
@@ -22,8 +42,6 @@ end
 
 
         end
-        def show
-
-        end
+       
 
 end 
