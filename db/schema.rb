36168f065580687bc9e99fdd93c90097e897aee2
Udit Chakraborty <udit.chakraborty8@gmail.com>
2018-11-14 15:33:10 +0530

    create users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_26_061145) do
+ActiveRecord::Schema.define(version: 2018_11_14_094935) do
 
   create_table "admissions", force: :cascade do |t|
     t.string "name"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
