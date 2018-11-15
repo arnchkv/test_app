90f20f0b9e4ff7ae6881eb79c3706d0319cdc456
Udit Chakraborty <udit.chakraborty8@gmail.com>
2018-11-15 11:48:24 +0530

    user model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
-    
-    
+    validates :username, presence: true,
+                 uniqueness:{case_sensitive: false}, 
+                 length: {minimum: 3, maximum: 50}
+                 VALID_EMAIL_REGEX: 
+    validates :email, presence: true, length {maximum: 100},
+                uniqueness:{case_sensitive: false},
+                 format: {with: VALID_EMAIL_REGEX }
 end
