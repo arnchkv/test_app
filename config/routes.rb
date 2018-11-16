73ee3329c4f37026f65583d5e0324045bfbf5773
Udit Chakraborty <udit.chakraborty8@gmail.com>
2018-11-16 14:31:45 +0530

    users page working

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   
   resources :admissions
+  get 'signup', to: "users#new"
+  resources :users, except: [:new]
 
+  #post 'users', to: "users#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
